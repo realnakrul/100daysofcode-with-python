d4795297b938c524eed5a73083d73305df7d32df
realnakrul <21193002+realnakrul@users.noreply.github.com>
2020-06-09 09:55:52 +0400

    Day 57 solution

--- UPLINK/api.py
+++ UPLINK/api.py
@@ -8,5 +8,13 @@ class MovieSearchClient(uplink.Consumer):
         super().__init__(base_url='http://movie_service.talkpython.fm/')
 
     @uplink.get('/api/search/{keyword}')
-    def search(self, keyword):
+    def search_by_keyword(self, keyword):
         """ Get's all movies by keyword. """
+
+    @uplink.get('/api/director/{director_name}')
+    def search_by_director(self, director_name):
+        """ Get's all movies by director name. """
+
+    @uplink.get('/api/movie/top')
+    def top(self):
+        """ Get's top 5 movies """
--- UPLINK/program.py
+++ UPLINK/program.py
@@ -4,12 +4,50 @@ from api import MovieSearchClient
 def search_by_keyword():
     svc = MovieSearchClient()
     keyword = input('Enter keyword to search: ')
-    response = svc.search(keyword)
-    print(response)
+    response = svc.search_by_keyword(keyword)
+    # print(response)
     if response.json():
-        for hit in response.json()['hits']:
-            print(hit['title'])
+        print_results(response.json())
+
+
+def search_by_director():
+    svc = MovieSearchClient()
+    director_name = input('Enter director name to search: ')
+    response = svc.search_by_director(director_name)
+    # print(response)
+    if response.json():
+        print_results(response.json())
+
+
+def top():
+    svc = MovieSearchClient()
+    # director_name = input('Enter director name to search: ')
+    response = svc.top()
+    # print(response)
+    if response.json():
+        print_results(response.json())
+
+
+def print_results(response):
+    print(f'Found {len(response["hits"])} item(s):')
+    if response['hits']:
+        for index, hit in enumerate(response['hits'], 1):
+            print(f'\t{index}. {hit["title"]} ({hit["year"]}) by {hit["director"]}')
 
 
 if __name__ == '__main__':
-    search_by_keyword()
+    print('*** Welcome to Movie search API with UPLINK Demo ***')
+    options = {
+        'Top 10 Movies': top,
+        'Search by keyword': search_by_keyword,
+        'Search by director': search_by_director
+    }
+    for index, option in enumerate(options, 1):
+        print(f'\t{index}. {option}')
+
+    service = int(input(f'Select service (1-{len(options)}): '))
+    option = list(options.keys())[service-1]
+    print(option)
+    options[option]()  # Calls function from options dictionary
+
+    # TODO Add view item
